51f40c6a262591acb29b805ee49896c854bcc276
Wooyong Chung <wychung78@gmail.com>
2022-07-18 12:02:29 +0900

    symbolic_link_fix

--- distrubution/1.3/make_symbolic_links.bat
+++ distrubution/1.3/make_symbolic_links.bat
@@ -1,11 +1,11 @@
 echo "If failed, then please remove created files"
 
-mklink /D runtime_data\keypoints    ..\1.0\runtime_data\keypoints
-mklink /D runtime_data\ocr          ..\1.0\runtime_data\ocr
-mklink /D runtime_data\postprocess  ..\1.0\runtime_data\postprocess
-mklink /D runtime_data\region       ..\1.0\runtime_data\region
-mklink /D runtime_data\runtime_data ..\1.0\runtime_data\runtime_data
-mklink    runtime_data\cameras.yaml ..\1.0\runtime_data\cameras.yaml
+mklink /D runtime_data\keypoints    ..\..\1.0\runtime_data\keypoints
+mklink /D runtime_data\ocr          ..\..\1.0\runtime_data\ocr
+mklink /D runtime_data\postprocess  ..\..\1.0\runtime_data\postprocess
+mklink /D runtime_data\region       ..\..\1.0\runtime_data\region
+mklink /D runtime_data\runtime_data ..\..\1.0\runtime_data\runtime_data
+mklink    runtime_data\cameras.yaml ..\..\1.0\runtime_data\cameras.yaml
 
 mklink liblept-DLL.dll ..\1.0\liblept-DLL.dll
 mklink libtesseract-DLL.dll ..\1.0\libtesseract-DLL.dll
