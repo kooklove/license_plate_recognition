eeef35688773762a34fd18e8d7ac9f9120cb5db0
sangdongahn <sangdong.ahn@gmail.com>
2022-07-19 19:41:24 +0900

    Add avg queries per sec on server metrics

--- front-end/src/pages/Server.js
+++ front-end/src/pages/Server.js
@@ -16,13 +16,15 @@ import Iconify from '../components/Iconify';
 
 // ----------------------------------------------------------------------
 const SERVER_URL = 'https://10.58.2.34:3503/performance';
+const SERVER_URL_STARTDATE = 'https://10.58.2.34:3503/performance/startdate';
 
 const TABLE_HEAD = [
 	{ id: 'id', label: 'User ID', alignRight: false },
 	{ id: 'number_total_query', label: '#Queries', alignRight: false },
-	{ id: 'number_exact_match', label: '#Exact natch', alignRight: false },
+	{ id: 'number_exact_match', label: '#Exact match', alignRight: false },
 	{ id: 'number_partial_match', label: '#Partial match', alignRight: false },
 	{ id: 'number_no_match', label: '#No match', alignRight: false },
+	{ id: 'avgNumberOfQueriesPerSec', label: "Avg #Query(sec)", alignRight: false }, 
 	{ id: '' },
 ];
 
@@ -59,6 +61,7 @@ function applySortFilter(array, comparator, query) {
 
 export default function Server() {
 	const [userlist, setUserlist] = useState(undefined);
+	const [serverStarttime, setServerstarttime] = useState(undefined);
 	const [page, setPage] = useState(0);
 	const [order, setOrder] = useState('asc');
 	const [selected, setSelected] = useState([]);
@@ -76,7 +79,8 @@ export default function Server() {
 		const today = new Date(+new Date() + 3240 * 10000).toISOString().replace("T", " ").replace(/\..*/, '');
 		setLastUpdated(today);
 
-		requestToPerformanceMetric();
+		requestToServerStarttime();
+		// requestToPerformanceMetric();
 	}, []);
 
 	useEffect(() => {
@@ -162,6 +166,56 @@ export default function Server() {
 			});
 	}
 
+	const requestToServerStarttime = async () => {
+		console.log("requestToServerStarttime");
+		axios
+			.get(SERVER_URL_STARTDATE)
+			.then(response => {
+				console.log(response);
+				const serverStartTime = response.data;
+				
+				setServerstarttime(serverStartTime);
+//
+				let serverDuration = new Date().getTime();
+				serverDuration -= serverStartTime;
+				serverDuration /= 1000;
+				
+				console.log("requestToPerformanceMetric");
+				axios
+					.get(SERVER_URL)
+					.then(response => {
+						console.log(response);
+						const data = response.data;
+						const total = {
+							id: "TOTAL",
+							number_total_query: 0,
+							number_exact_match: 0,
+							number_partial_match: 0,
+							number_no_match: 0,
+							avgNumberOfQueriesPerSec : 0.0
+						};
+						data.forEach((d) => {
+							total.number_total_query += d.number_total_query;
+							total.number_exact_match += d.number_exact_match;
+							total.number_partial_match += d.number_partial_match;
+							total.number_no_match += d.number_no_match;
+							total.avgNumberOfQueriesPerSec =(total.number_total_query / serverDuration);
+							d.avgNumberOfQueriesPerSec =  (d.number_total_query / serverDuration);
+
+						});
+						data.unshift(total);
+						setUserlist(data);
+					})
+					.catch(err => {
+						console.log('err', err);
+					});
+//
+			})
+			.catch(err => {
+				console.log('err', err);
+			});
+	}
+
 	return (<>
 		{userlist &&
 			<Page title="Server">
@@ -200,7 +254,8 @@ export default function Server() {
 									<TableBody>
 										{filteredUsers && filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => {
 											// const { id, numTotalQuery, numExactMatch, numPartialMatch, numNoMatch } = row;
-											const { id, number_total_query, number_exact_match, number_partial_match, number_no_match } = row;
+											const { id, number_total_query, number_exact_match, number_partial_match, number_no_match, avgNumberOfQueriesPerSec } = row;
+											const convertNumber = Number(avgNumberOfQueriesPerSec).toFixed(4);
 
 											const isItemSelected = selected.indexOf(id) !== -1;
 											const bcolor = (index === 0 ? 'red' : 'black');
@@ -234,6 +289,7 @@ export default function Server() {
 													<TableCell align="left" style={{ color: bcolor }}>{number_exact_match}</TableCell>
 													<TableCell align="left" style={{ color: bcolor }}>{number_partial_match}</TableCell>
 													<TableCell align="left" style={{ color: bcolor }}>{number_no_match}</TableCell>
+													<TableCell align="left" style={{ color: bcolor }}>{convertNumber}</TableCell>
 													<TableCell align="right">
 														<UserMoreMenu />
 													</TableCell>
